dcdcc6eb3c27c213fd30683fc903d56ae51aaec4
Aromasodu Okikiola <oaromasodu@gmail.com>
2023-03-20 06:32:23 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main function to print a C program using puts
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
