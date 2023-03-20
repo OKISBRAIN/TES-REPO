5e22d627f766822433fce1d45b81dc0de86c7f90
Aromasodu Okikiola <oaromasodu@gmail.com>
2023-03-20 06:38:19 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main function is to print a sentence as it is in a new line
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
